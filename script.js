3cf37149370161ece674793dd46450036214921b
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-03 00:49:29 +0100

    zadanie 8.5 add

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+//Area of a triangle calculator
+var a = prompt('Enter number for triangle base a' ),
+		h = prompt('Enter number for triangle height h' ),
+		triangleArea = a * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
